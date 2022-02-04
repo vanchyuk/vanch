60173cead5ff02042b21490c8efe674971161b1c
alex <g@gmail.com>
2022-02-04 18:59:00 +0800

    Init

---
+++ js/script.js
@@ -0,0 +1,11 @@
+$(document).ready(function(){
+    $('.carousel__inner').slick({
+        dots: true,
+        infinite: true,
+        speed: 300,
+        slidesToShow: 1,
+        centerMode: true,
+        variableWidth: true
+      });
+  });
+          
